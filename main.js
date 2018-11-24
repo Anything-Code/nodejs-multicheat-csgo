67c3bdbe0691e16212e71236bcef4d8deb7a8a3a
Niklas Lübcke <niklas@lubcke.de>
2018-11-24 23:00:51 +0100

    added toggle functionality

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const memoryjs = require('memoryjs')
+const iohook = require('iohook')
+const keycode = require('keycode')
 
 const offsets = {
     m_iGlowIndex: 0x0000A344,
@@ -7,18 +9,34 @@ const offsets = {
     dwGlowObjectManager: 0x51809B0
 }
 
-const   processObject = memoryjs.openProcess("csgo.exe"),
-        processHandle = processObject.handle,
-        clientModule = memoryjs.findModule("client_panorama.dll", processObject.th32ProcessID),
-        client = clientModule.modBaseAddr
+const processObject = memoryjs.openProcess("csgo.exe")
+const processHandle = processObject.handle
+const clientModule = memoryjs.findModule("client_panorama.dll", processObject.th32ProcessID)
+const client = clientModule.modBaseAddr
+
+console.log('glow started...')
+let activated = true
+
+iohook.on('keydown', keyMeta => {
+    if (keycode(keyMeta.rawcode) == 'f12') {
+        if (activated) {
+            activated = false
+            console.log('glow terminated...')
+        } else {
+            activated = true
+            console.log('glow started...')
+        }
+    }
+})
+iohook.start()
 
 setInterval(() => {
-    if (memoryjs.readMemory(processHandle, client + offsets.dwEntityList, 'dword') > 0) {
+    if (memoryjs.readMemory(processHandle, client + offsets.dwEntityList, 'dword') > 0 && activated) {
         let glow_manager = memoryjs.readMemory(processHandle, client + offsets.dwGlowObjectManager, 'int')
 
         for (let i = 1; i < 32; i++) {
             let entity = memoryjs.readMemory(processHandle, client + offsets.dwEntityList + i * 0x10, 'int')
-            
+
             if (entity) {
                 let entity_team_id = memoryjs.readMemory(processHandle, entity + offsets.m_iTeamNum, 'int'),
                     entity_glow = memoryjs.readMemory(processHandle, entity + offsets.m_iGlowIndex, 'int')
@@ -30,14 +48,14 @@ setInterval(() => {
                         memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0xC), 0, 'float')
                         memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x10), 1, 'float')
                         memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x24), 1, 'int')
-                    break;
+                        break;
                     case 3: //Counter Terrorist
                         memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x4), 0, 'float')
                         memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x8), 0, 'float')
                         memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0xC), 1, 'float')
                         memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x10), 1, 'float')
                         memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x24), 1, 'int')
-                    break;
+                        break;
                 }
             }
         }
