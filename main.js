b2c3e8c40cde7a1c6cca4e76045bd5ff9b15e9a2
Niklas Lübcke <niklas@lubcke.de>
2018-11-25 01:15:05 +0100

    split into Glow-Class

--- main.js
+++ main.js
@@ -1,63 +1,20 @@
-const memoryjs = require('memoryjs')
-const iohook = require('iohook')
-const keycode = require('keycode')
+const GLOW = require('./glow')
+const KEYBOARD = require('iohook')
+const KEYCODE = require('keycode')
 
-const offsets = {
-    m_iGlowIndex: 0x0000A344,
-    m_iTeamNum: 0x000000F0,
-    dwEntityList: 0x4C41704,
-    dwGlowObjectManager: 0x51809B0
-}
-
-const processObject = memoryjs.openProcess("csgo.exe")
-const processHandle = processObject.handle
-const clientModule = memoryjs.findModule("client_panorama.dll", processObject.th32ProcessID)
-const client = clientModule.modBaseAddr
-
-let mainLoop,
-    activated = true
-
-iohook.on('keydown', keyMeta => {
-    if (keycode(keyMeta.rawcode) == 'f12') {
-        if (activated) {
-            clearInterval(mainLoop)
-            activated = false
+KEYBOARD.on('keydown', keyMeta => {
+    if (KEYCODE(keyMeta.rawcode) === 'f12') {
+        if (Glow.activated) {
+            clearInterval(Glow.loopInterval)
+            Glow.activated = false
             console.log('glow terminated...')
         } else {
             console.log('glow started...')
-            activated = true
-            mainLoop = setInterval(() => {
-                if (memoryjs.readMemory(processHandle, client + offsets.dwEntityList, 'dword') > 0) {
-                    let glow_manager = memoryjs.readMemory(processHandle, client + offsets.dwGlowObjectManager, 'int')
-            
-                    for (let i = 1; i < 32; i++) {
-                        let entity = memoryjs.readMemory(processHandle, client + offsets.dwEntityList + i * 0x10, 'int')
-            
-                        if (entity) {
-                            let entity_team_id = memoryjs.readMemory(processHandle, entity + offsets.m_iTeamNum, 'int'),
-                                entity_glow = memoryjs.readMemory(processHandle, entity + offsets.m_iGlowIndex, 'int')
-            
-                            switch (entity_team_id) {
-                                case 2: // Terrorist
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x4), 1, 'float')
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x8), 0, 'float')
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0xC), 0, 'float')
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x10), 1, 'float')
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x24), 1, 'int')
-                                    break;
-                                case 3: //Counter Terrorist
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x4), 0, 'float')
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x8), 0, 'float')
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0xC), 1, 'float')
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x10), 1, 'float')
-                                    memoryjs.writeMemory(processHandle, glow_manager + (entity_glow * 0x38 + 0x24), 1, 'int')
-                                    break;
-                            }
-                        }
-                    }
-                }
-            })
+            Glow = new GLOW
         }
     }
 })
-iohook.start()
+KEYBOARD.start()
+
+console.log('glow started...')
+let Glow = new GLOW
