097e7f1fc81aad7c7581c6bb651ed2803159c3e0
Niklas Lübcke <niklas@lubcke.de>
2018-11-25 21:28:29 +0100

    added functionality to start cheat before 'csgo.exe'

--- glow.js
+++ glow.js
@@ -1,34 +1,34 @@
-const OFFSETS = require('./OFFSETS')
-const MEMORY = require('memoryjs')
-
-const PROCESS_OBJECT = MEMORY.openProcess("csgo.exe")
-const PROCESS_HANDLE = PROCESS_OBJECT.handle
-const CLIENT_MODULE = MEMORY.findModule("client_panorama.dll", PROCESS_OBJECT.th32ProcessID)
-const CLIENT = CLIENT_MODULE.modBaseAddr
+const Offsets = require('./offsets')
+const Memory = require('memoryjs')
 
 module.exports = class Glow {
-    constructor () {
-        this.activated = true
-        this.mainLoop()
+    constructor (processObject) {
+        this.processObject = processObject
+        this.processHandle = this.processObject.handle
+        this.clientModule = Memory.findModule("client_panorama.dll", this.processObject.th32ProcessID)
+        this.client = this.clientModule.modBaseAddr
+        this.enable()
     }
-    mainLoop () {
+    enable () {
+        this.activated = true
+        console.log('glow enabled...')
         this.loopInterval = setInterval(() => {
-            if (MEMORY.readMemory(PROCESS_HANDLE, CLIENT + OFFSETS.dwEntityList, 'dword') > 0) {
-                let glow_manager = MEMORY.readMemory(PROCESS_HANDLE, CLIENT + OFFSETS.dwGlowObjectManager, 'int')
+            if (Memory.readMemory(this.processHandle, this.client + Offsets.dwEntityList, 'dword') > 0) {
+                let glow_manager = Memory.readMemory(this.processHandle, this.client + Offsets.dwGlowObjectManager, 'int')
         
                 for (let i = 1; i < 32; i++) {
-                    let entity = MEMORY.readMemory(PROCESS_HANDLE, CLIENT + OFFSETS.dwEntityList + i * 0x10, 'int')
+                    let entity = Memory.readMemory(this.processHandle, this.client + Offsets.dwEntityList + i * 0x10, 'int')
         
                     if (entity) {
-                        let entity_team_id = MEMORY.readMemory(PROCESS_HANDLE, entity + OFFSETS.m_iTeamNum, 'int'),
-                            entity_glow = MEMORY.readMemory(PROCESS_HANDLE, entity + OFFSETS.m_iGlowIndex, 'int')
+                        let entity_team_id = Memory.readMemory(this.processHandle, entity + Offsets.m_iTeamNum, 'int'),
+                            entity_glow = Memory.readMemory(this.processHandle, entity + Offsets.m_iGlowIndex, 'int')
         
                         switch (entity_team_id) {
                             case 2: // Terrorist
-                                this.enableGlow(glow_manager, entity_glow, 1, 0, 0, 1)
+                                this.drawOutline(glow_manager, entity_glow, 1, 0, 0, 1)
                             break;
                             case 3: //Counter Terrorist
-                                this.enableGlow(glow_manager, entity_glow, 0, 0, 1, 1)
+                                this.drawOutline(glow_manager, entity_glow, 0, 0, 1, 1)
                             break;
                         }
                     }
@@ -36,11 +36,16 @@ module.exports = class Glow {
             }
         })
     }
-    enableGlow (glow_manager, entity_glow, r, g, b, a) {
-        MEMORY.writeMemory(PROCESS_HANDLE, glow_manager + (entity_glow * 0x38 + 0x4), r, 'float')
-        MEMORY.writeMemory(PROCESS_HANDLE, glow_manager + (entity_glow * 0x38 + 0x8), g, 'float')
-        MEMORY.writeMemory(PROCESS_HANDLE, glow_manager + (entity_glow * 0x38 + 0xC), b, 'float')
-        MEMORY.writeMemory(PROCESS_HANDLE, glow_manager + (entity_glow * 0x38 + 0x10), a, 'float')
-        MEMORY.writeMemory(PROCESS_HANDLE, glow_manager + (entity_glow * 0x38 + 0x24), 1, 'int')
+    drawOutline (glow_manager, entity_glow, r, g, b, a) {
+        Memory.writeMemory(this.processHandle, glow_manager + (entity_glow * 0x38 + 0x4), r, 'float')
+        Memory.writeMemory(this.processHandle, glow_manager + (entity_glow * 0x38 + 0x8), g, 'float')
+        Memory.writeMemory(this.processHandle, glow_manager + (entity_glow * 0x38 + 0xC), b, 'float')
+        Memory.writeMemory(this.processHandle, glow_manager + (entity_glow * 0x38 + 0x10), a, 'float')
+        Memory.writeMemory(this.processHandle, glow_manager + (entity_glow * 0x38 + 0x24), 1, 'int')
+    }
+    disable () {
+        clearInterval(this.loopInterval)
+        this.activated = false
+        console.log('glow disabled...')
     }
 }
--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-const GLOW = require('./glow')
-const KEYBOARD = require('iohook')
-const KEYCODE = require('keycode')
+const Glow = require('./glow')
+const Memory = require('memoryjs')
+const Keyboard = require('iohook')
+const Keycode = require('keycode')
 
-KEYBOARD.on('keydown', keyMeta => {
-    if (KEYCODE(keyMeta.rawcode) === 'f12') {
-        if (Glow.activated) {
-            clearInterval(Glow.loopInterval)
-            Glow.activated = false
-            console.log('glow terminated...')
-        } else {
-            console.log('glow started...')
-            Glow = new GLOW
-        }
+class Main {
+    constructor () {
+        this.started = false
+        this.gameClosedAtStart = true
+        this.listenForGameState()
     }
-})
-KEYBOARD.start()
-
-console.log('glow started...')
-let Glow = new GLOW
+    listenForGameState () {
+        this.processStateInterval = setInterval(() => {
+            if (this.processFound('csgo.exe') && !this.started) {
+                this.started = true
+                console.log('"csgo.exe" found')
+                this.listenForKeyboardInput(this.process)
+            } else if (!this.processFound('csgo.exe') && this.started) {
+                this.started = false
+                this.gameClosedAtStart = false
+                console.log('"csgo.exe" was closed. Terminating Process...')
+                process.exit()
+            } else if (!this.processFound('csgo.exe') && !this.started && this.gameClosedAtStart) {
+                this.gameClosedAtStart = false
+                console.log('"csgo.exe" not found, please start "csgo.exe"')
+            }
+        }, 200)
+    }
+    processFound (processName) {
+        this.process = Memory.openProcess(processName)
+        if (this.process.szExeFile == processName)
+            return true
+        else
+            return false
+    }
+    listenForKeyboardInput (process) {
+        console.log('Press f12 to toggle glow...')
+        Keyboard.on('keydown', key => {
+            if (Keycode(key.rawcode) === 'f12' && this.started) {
+                if (this.glow === undefined)
+                    this.glow = new Glow(process)
+                else if (this.glow.activated)
+                    this.glow.disable()
+                else
+                    this.glow.enable()
+            }
+        })
+        Keyboard.start()
+    }
+}
+new Main
